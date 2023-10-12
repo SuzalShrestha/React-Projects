44ec4a23a5fb276ab8ed9919d7033c53ecbe155e
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-10-12 14:17:11 +0545

    added states for input and select in form

--- Travel-list/src/App.jsx
+++ Travel-list/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,10 +22,37 @@ function Logo() {
   return <h1>Far Away</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setquantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+    console.log(newItem);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Where do you wanna go?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setquantity(+e.target.value);
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((item, i) => {
+          return (
+            <option value={item} key={i}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        placeholder="items..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>ADD</button>
+    </form>
   );
 }
 function PackingList() {
