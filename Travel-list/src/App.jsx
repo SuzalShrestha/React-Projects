5d5e76070437fbeee75b7288aca4d265199e0778
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-12-31 21:38:27 +0545

    added sorting button

--- Travel-list/src/App.jsx
+++ Travel-list/src/App.jsx
@@ -80,10 +80,18 @@ function Form({ onAddItem }) {
   );
 }
 function PackingList({ items, onRemoveItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  else if (sortBy === "description")
+    sortedItems = items.sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  else sortedItems = items.sort((a, b) => a.packed - b.packed);
   return (
     <div className="list">
       <ul>
-        {items.map((item) => {
+        {sortedItems.map((item) => {
           return (
             <Item
               item={item}
@@ -94,6 +102,13 @@ function PackingList({ items, onRemoveItem, onToggleItem }) {
           );
         })}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input Order</option>
+          <option value="description">Sort By Description</option>
+          <option value="packed">Sort By Packed</option>
+        </select>
+      </div>
     </div>
   );
 }
