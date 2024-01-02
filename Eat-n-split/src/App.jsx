12b03bd4ef957db42cc535241b8e77d7926abd71
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2024-01-02 10:03:33 +0545

    building static ui

--- Eat-n-split/src/App.jsx
+++ Eat-n-split/src/App.jsx
@@ -22,20 +22,23 @@ const initialFriends = [
 function App() {
   return (
     <div className="app">
-      <FriendsList />
+      <div className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
+      </div>
+      <FormSplitBill />
     </div>
   );
 }
 function FriendsList() {
   const [friends, setFriends] = useState(initialFriends);
   return (
-    <div className="sidebar">
-      <ul>
-        {friends.map((friend) => {
-          return <Friend friend={friend} key={friend.id} />;
-        })}
-      </ul>
-    </div>
+    <ul>
+      {friends.map((friend) => {
+        return <Friend friend={friend} key={friend.id} />;
+      })}
+    </ul>
   );
 }
 
@@ -46,17 +49,50 @@ function Friend({ friend }) {
       <h3>{friend.name}</h3>
       {friend.balance > 0 ? (
         <p className="green">
-          `${friend.name} owes you ${friend.balance}`
+          {friend.name} owes you {friend.balance}
         </p>
       ) : friend.balance < 0 ? (
         <p className="red">
-          `You owe your friend ${friend.name} ${Math.abs(friend.balance)}`
+          You owe your friend {friend.name} {Math.abs(friend.balance)}
         </p>
       ) : (
-        <p>`${friend.name} is all even with you`</p>
+        <p>{friend.name} is all even with you</p>
       )}
       <button className="button">Select</button>
     </li>
+  );
+}
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split Bill</h2>
+      <label>💰 Bill Value</label>
+      <input type="text"></input>
+      <label>🧑 Your expenses</label>
+      <input type="text"></input>
+      <label>🧑‍🤝‍🧑 X's expenses</label>
+      <input type="text" disabled></input>
+      <label>💲 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
+  );
+}
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑🏼‍🤝‍🧑🏻 Friend Name</label>
+      <input type="text"></input>
+      <label>🖼️ Image URL</label>
+      <input type="text"></input>
+      <Button>Add</Button>
+    </form>
   );
 }
 
