7ae20ccf2f24bf0ea8b5ade570b5e5af19f77d0b
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2024-01-16 09:56:06 +0545

    how react works initial commit

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import "./index.css";
-import StarRating from "./StarRating.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating />
+    <App />
   </React.StrictMode>
 );
